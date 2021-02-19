920d64ca5ecca42f06edea58b9b36e7c69f6002f
Lafolie <lafoliex7@aol.com>
2021-02-19 23:38:51 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Choco-Driller
+
+Entry for [LÖVE Jam 2021](https://itch.io/jam/love2d-jam-2021).
+
+*Requires LÖVE 11.3*
---
+++ main.lua
@@ -0,0 +1,51 @@
+class = require "lib.class"
+vector = require "lib.brinevector.brinevector"
+
+-------------------------------------------------------------------------------
+-- Main Callbacks
+-------------------------------------------------------------------------------
+
+function love.load()
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+function love.quit()
+
+end
+
+-------------------------------------------------------------------------------
+-- Keyboard
+-------------------------------------------------------------------------------
+
+function love.keypressed(key, scan, isRepeat)
+
+end
+
+function love.keyreleased(key, scan)
+
+end
+
+-------------------------------------------------------------------------------
+-- Mouse
+-------------------------------------------------------------------------------
+
+function love.mousemoved(x, y, dx, dy)
+
+end
+
+function love.mousepressed(x, y, btn)
+
+end
+
+function love.mousereleased(x, y, btn)
+
+end
+
